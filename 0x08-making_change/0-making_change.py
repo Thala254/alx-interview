ee01a52ce3bbc0a1d1593d2e0d4fdcbaf5334993
thala254 <thalamine@gmail.com>
2023-02-23 22:34:31 +0300

    added 0x08-making_change interview project file and a readme

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Given a pile of coins of diggerent values,
+determine the fewest number of coins needed to meet a given a mount ototal
+"""
+import sys
+
+
+def makeChange(coins, total):
+    """
+    Function that determines coin change
+    :returns
+        - fewest number of coins needed to meet total
+    if total is 0 or less, return 0
+    if total cannot be met by any number of coins you have, return -1
+    """
+    if total <= 0:
+        return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
+
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
